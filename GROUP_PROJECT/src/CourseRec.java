0137881ae7ff340fed6b91c219add5cdbf33c656
ACKEEM <ACKEEM@10.20.122.51>
2018-11-23 19:47:20 -0500

    sss

--- GROUP_PROJECT/src/CourseRec.java
+++ GROUP_PROJECT/src/CourseRec.java
@@ -1,5 +1,10 @@
 
 public class CourseRec {
+  
+		//	may need to extend
+	// Done already 
+	//
+	//
 	
 	private String code;
 	private String name;
@@ -7,84 +12,94 @@ public class CourseRec {
 	private int credit;
 	private Date startDate;
 	private Date endDate;
-	
-	
-	
+
 	public CourseRec() {
-		setCode("CodeForCourse");              
-		setName("name");              
+		setCode("CodeForCourse");
+		setName("name");
 		setDescription("description");
-		setCredit(1000);                      
-		setStartDate(new Date(01,01,2018));    
-		setEndDate(new Date(01,01,2018));        
-		                            
+		setCredit(1000);
+		setStartDate(new Date(01, 01, 2018));
+		setEndDate(new Date(01, 01, 2018));
+
 	}
-	
-	
+
 	public CourseRec(String code, String name, String description, int credit, Date startDate, Date endDate) {
-		//super();
+		// super();
 		setCode(code);
 		setName(name);
 		setDescription(description);
-		setCredit(credit);                                                                                                                                                                                                                                                                                  
+		setCredit(credit);
 		setStartDate(startDate);
 		setEndDate(endDate);
 	}
-	
-	
+
 	public CourseRec(CourseRec obj) {
 		setCode(obj.getCode());
 		setName(obj.getName());
 		setDescription(obj.getDescription());
 		setCredit(obj.getCredit());
-		
-		
+		setStartDate(obj.getStartDate());
+		setEndDate(obj.getEndDate());
 	}
-	
-	
+
 	public String getCode() {
 		return code;
 	}
+
 	public void setCode(String code) {
 		this.code = code;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public int getCredit() {
 		return credit;
 	}
+
 	public void setCredit(int credit) {
 		this.credit = credit;
 	}
+
 	public Date getStartDate() {
 		return startDate;
 	}
+
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
+
 	public Date getEndDate() {
 		return endDate;
 	}
+
 	public void setEndDate(Date endDate) {
 		this.endDate = endDate;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	public String toString() {
+		String out;
+		out = getCode() + "CODE: " + "\n";
+		out += getName() + "NAME: " + "\n";
+		out += getDescription() + "Description: " + "\n";
+		out += getCredit() + "CREDIT: " + "\n";
+		out += getStartDate() + "END DATE: " + "\n";
+		out += getEndDate() + "END DATE: " + "\n";
+
+		return out;
+	}
+
 }
